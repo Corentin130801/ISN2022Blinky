69425afa0e239f3812a2abbc18ef6c02d2a16267
Leo <leolin.fr@gmail.com>
2022-10-20 20:12:07 +0200

    test leo

--- Blinky/src/game/Main.java
+++ Blinky/src/game/Main.java
@@ -1,23 +1,23 @@
 package game;
 
-import javax.swing.JFrame; // Cela ajoute une fenêtre avec une bordure et une barre de titre 
+import javax.swing.JFrame; // Cela ajoute une fenï¿½tre avec une bordure et une barre de titre 
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // setDefaultCloseOperation permet de définir ce qu'il se passe quan on appuie sur la croix.
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // setDefaultCloseOperation permet de dï¿½finir ce qu'il se passe quan on appuie sur la croix.
 		window.setTitle("Blinky"); // pour afficher le titre
-		window.setResizable(false); // permet de donner la taille qu'on veut à la fenêtre sans qu'elle soit redimensionnée
-		window.setLocationRelativeTo(null); // pas de location spécifiée, windows la placera au centre
+		window.setResizable(false); // permet de donner la taille qu'on veut ï¿½ la fenï¿½tre sans qu'elle soit redimensionnï¿½e
+		window.setLocationRelativeTo(null); // pas de location spï¿½cifiï¿½e, windows la placera au centre
 		window.setVisible(true);
 		
 		GamePlan gamePlan = new GamePlan();
 		window.add(gamePlan);
-		window.pack(); // pour pouvoir voir la fenêtre
+		window.pack(); // pour pouvoir voir la fenï¿½tre
 		//ceci est un test 
-		
+		//test leo
 
 }
 }
