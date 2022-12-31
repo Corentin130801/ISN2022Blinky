47182a177bd49a66cd1164328164b0bbb76da428
leo lin <leolin.fr@gmail.com>
2022-12-31 13:35:37 +0100

    10 monstres

--- BlinkyMaven/src/main/java/game/Setter.java
+++ BlinkyMaven/src/main/java/game/Setter.java
@@ -70,8 +70,50 @@ public class Setter {
     	nouveaujeu.Fmonstre[0].worldX = 22*nouveaujeu.tailleCarreaux;
     	nouveaujeu.Fmonstre[0].worldY = 7*nouveaujeu.tailleCarreaux;
     	
+    	nouveaujeu.Fmonstre[1]= new Fantome(nouveaujeu);
+    	nouveaujeu.Fmonstre[1].worldX = 21*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.Fmonstre[1].worldY = 12*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.Fmonstre[2]= new Fantome(nouveaujeu);
+    	nouveaujeu.Fmonstre[2].worldX = 24*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.Fmonstre[2].worldY = 15*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.Fmonstre[3]= new Fantome(nouveaujeu);
+    	nouveaujeu.Fmonstre[3].worldX = 45*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.Fmonstre[3].worldY = 42*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.Fmonstre[4]= new Fantome(nouveaujeu);
+    	nouveaujeu.Fmonstre[4].worldX = 32*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.Fmonstre[4].worldY = 34*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.Fmonstre[5]= new Fantome(nouveaujeu);
+    	nouveaujeu.Fmonstre[5].worldX = 27*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.Fmonstre[5].worldY = 24*nouveaujeu.tailleCarreaux;
+    	
+    	
+    	
     	nouveaujeu.monstre[0]= new Bonhomme(nouveaujeu);
     	nouveaujeu.monstre[0].worldX = 21*nouveaujeu.tailleCarreaux;
     	nouveaujeu.monstre[0].worldY = 7*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.monstre[1]= new Bonhomme(nouveaujeu);
+    	nouveaujeu.monstre[1].worldX = 15*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.monstre[1].worldY = 21*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.monstre[2]= new Bonhomme(nouveaujeu);
+    	nouveaujeu.monstre[2].worldX = 22*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.monstre[2].worldY = 24*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.monstre[3]= new Bonhomme(nouveaujeu);
+    	nouveaujeu.monstre[3].worldX = 37*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.monstre[3].worldY = 42*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.monstre[4]= new Bonhomme(nouveaujeu);
+    	nouveaujeu.monstre[4].worldX = 34*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.monstre[4].worldY = 34*nouveaujeu.tailleCarreaux;
+    	
+    	nouveaujeu.monstre[5]= new Bonhomme(nouveaujeu);
+    	nouveaujeu.monstre[5].worldX = 30*nouveaujeu.tailleCarreaux;
+    	nouveaujeu.monstre[5].worldY = 24*nouveaujeu.tailleCarreaux;
     }
 }
